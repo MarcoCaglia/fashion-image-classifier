866d96073d84fbe984c7824048b67ed1881f38f7
marco <cagliamarco@gmail.com>
2021-02-12 08:37:49 +0100

    Feature: Started Image Feature Extractor

---
+++ fashion_image_classifier/feature_extraction.py
@@ -0,0 +1,22 @@
+"""Module to extract Features from Images."""
+
+import numpy as np
+from PIL import Image
+from typing import Tuple
+
+
+class FeatureExtractor:
+    """Class for extracting Features from Images."""
+
+    def __init__(
+        self,
+        greyscale: bool = False,
+        aspect_ration: Tuple[int, int] = None,
+
+    ):
+        """Initialize Feature Extractor."""
+        pass
+
+    def extract_features(self):
+        """Extract Features from images."""
+        pass
