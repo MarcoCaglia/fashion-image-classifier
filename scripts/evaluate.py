a847c9e901fef909554f52b9c517fac045be52ea
MarcoCaglia <cagliamarco@gmail.com>
2021-02-19 08:05:20 +0100

    Feature Finished DVC pipeline

---
+++ scripts/evaluate.py
@@ -0,0 +1,44 @@
+"""Module to evaluate the previously trained model."""
+
+import numpy as np
+import pandas as pd
+from joblib import load
+from sklearn.metrics import (accuracy_score, confusion_matrix, f1_score,
+                             precision_score, recall_score)
+import seaborn as sns
+
+from scripts.prepare import SAVE_PATH
+
+X_TEST_PATH = SAVE_PATH.joinpath("test_features.npy")
+Y_TEST_PATH = SAVE_PATH.joinpath("test_labels.npy")
+
+MODEL_PATH = SAVE_PATH.joinpath("model.joblib")
+
+
+def evaluate():
+    """Evaluate the model."""
+    X_test = np.load(X_TEST_PATH)
+    y_test = np.load(Y_TEST_PATH)
+
+    model = load(MODEL_PATH)
+
+    y_hat = model.predict(X_test)
+
+    metrics = {
+        "Accuracy": accuracy_score(y_test, y_hat),
+        "Precision": precision_score(y_test, y_hat),
+        "Recall": recall_score(y_test, y_hat),
+        "F1-Score": f1_score(y_test, y_hat),
+    }
+
+    metrics = pd.DataFrame.from_dict(
+        metrics, orient="index", columns=["Test-Score"]
+        )
+    metrics.to_csv(SAVE_PATH.joinpath("model_test_results.csv"))
+
+    confusion = sns.heatmap(confusion_matrix(y_test, y_hat))
+    confusion.figure.savefig(SAVE_PATH.joinpath("model_test_confusion.png"))
+
+
+if __name__ == "__main__":
+    evaluate()
--- scripts/train.py
+++ scripts/train.py
@@ -1,25 +1,53 @@
 """Module to run model training after prepare.py has been run."""
 
-from .prepare import SAVE_PATH
+from pathlib import Path
+
 import numpy as np
-from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
+import yaml
+from joblib import dump
+from sklearn.decomposition import KernelPCA
 from sklearn.pipeline import Pipeline
-from sklearn.decomposition import Decomposition
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+
+from scripts.prepare import SAVE_PATH
 
 X_TRAIN_PATH = SAVE_PATH.joinpath("train_features.npy")
-y_TRAIN_PATH = SAVE_PATH.joinpath("train_labels.npy")
-X_TEST_PATH = SAVE_PATH.joinpath("test_features.npy")
-y_TEST_PATH = SAVE_PATH.joinpath("test_labels.npy")
+Y_TRAIN_PATH = SAVE_PATH.joinpath("train_labels.npy")
+
+PARAMS_PATH = Path(__file__).absolute().parent.parent.joinpath('params.yaml')
 
 
 def train_model():
     """Train the model."""
     X_train = np.load(X_TRAIN_PATH)
-    y_train = np.load(y_TRAIN_PATH)
-    X_test = np.load(X_TEST_PATH)
-    y_test = np.load(y_TEST_PATH)
+    y_train = np.load(Y_TRAIN_PATH)
+
+    with PARAMS_PATH.open("r") as f:
+        params = yaml.safe_load(f)["train"]
 
     model = Pipeline([
-        ("scaler", StandardScaler() if use_)
+        (
+            "scaler",
+            StandardScaler() if params["use_scaler"] else "passtrhough"
+            ),
+        ("pca", KernelPCA(
+            n_components=params["pca__n_components"],
+            gamma=params["pca__gamma"],
+            kernel=params["pca__kernel"]
+            )
+            if params["use_pca"] else "passthrough"),
+        ("classifier", SVC(
+            C=params["classifier__C"],
+            kernel=params["classifier__kernel"],
+            gamma=params["classifier__gamma"]
+            ))
     ])
+
+    _ = model.fit(X_train, y_train)
+
+    _ = dump(model, SAVE_PATH.joinpath("model.joblib"))
+
+
+if __name__ == "__main__":
+    train_model()
