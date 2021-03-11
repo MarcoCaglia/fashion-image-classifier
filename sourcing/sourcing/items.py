bdebb658862c44081fc8cb81b58df7dd7cc3177d
Marco Caglia <cagliamarco@gmail.com>
2021-03-11 06:58:37 +0100

    Revert "Feature/source review fields"

--- sourcing/sourcing/items.py
+++ sourcing/sourcing/items.py
@@ -10,8 +10,6 @@ class ZalandoItem(scrapy.Item):
     item_id = scrapy.Field()
     name = scrapy.Field()
     price = scrapy.Field()
-    rating = scrapy.Field()
-    reviews = scrapy.Field()
     brand = scrapy.Field()
     colour = scrapy.Field()
     image = scrapy.Field()
